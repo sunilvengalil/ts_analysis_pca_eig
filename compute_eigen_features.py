e87af3a6a6f8b9b9b0a3092ed94258a83a0764b7
sunilvengalil-rzt <vengalilsunilkumar@gmail.com>
2022-03-09 13:49:28 +0530

    Adding gt and tsne code

--- compute_eigen_features.py
+++ compute_eigen_features.py
@@ -7,6 +7,7 @@ import pandas as pd
 import argparse
 from matplotlib import pyplot as plt
 from utils import compute_svd, process_time_series_all, plot_eigen_ratio, area_per_unit_length
+from sklearn.manifold import TSNE
 
 MIN_EIGEN_VALUE = 10
 
@@ -22,6 +23,11 @@ MIN_EIGEN_VALUE = 10
 #                    help='Path for folder where datafiles are located.Can also be a filename in case of single time_series file')
 # args = parser.parse_args()
 # print(args)
+
+# file_names = ["Delta", "Sac_ascf_theta", "Sac_ascf_beta", "Sac_ascf_kai", "Lorenz", "Lordata", "Sac_ascf_lambda", "Kappa", "Sac_ascf_phi", "Sac_ascf_gamma", "Sac_Ascf_Alpha", "Sac_ascf_mu", "Sac_ascf_nu", "Sac_ascf_rho"]
+file_names = ["Delta", "Theta", "Beta", "Kai", "Lorenz", "Lordata", "Lambda", "Kappa", "Phi", "Gamma", "Alpha", "Mu", "Nu", "Rho"]
+ground_truths = ["STOCHASTIC", "NON-ST", "NON-ST", "STOCHASTIC", "NON-ST", "NON-ST", "NON-ST", "NON-ST", "STOCHASTIC", "STOCHASTIC", "NON-ST", "NON-ST", "NON-ST", "NON-ST"]
+gt_dictionary = {file_name:ground_truth for file_name, ground_truth in zip(file_names, ground_truths) }
 
 
 file_name = "time_series"
@@ -72,13 +78,27 @@ for file_name in data_dict.keys():
     normalized_area = area_per_unit_length(results, max_value)
 
     # insert into dictionary for creating dataframe
-
     values["File Name"].append(file_name)
     values["Max Eigen Ratio"].append(max_eig_ratio)
     values["Variance of Eigen Ratio"].append(var_eig_ratio)
     values["Normalized Area Under Eigen Ratio"].append(normalized_area)
-    values["Area Under Eigen Ratio"].append(normalized_area * max_value)
+    key = file_name.rsplit(".",  1)[0]
+    print(key)
+    if key.startswith("sac_ascf_"):
+        key = key.replace("sac_ascf_", "")
+    print(key)
+    values["Ground Truth"].append(gt_dictionary[key.capitalize()])
 
-#df = pd.DataFrame(values, columns=["File Name", "Maximum Eigen Ratio", "Variance of Eigen Ratio", "Normalized Area Under Eigen Ratio", "Area Under Eigen Ratio"])
+
+# df = pd.DataFrame(values, columns=["File Name", "Maximum Eigen Ratio", "Variance of Eigen Ratio", "Normalized Area Under Eigen Ratio", "Area Under Eigen Ratio"])
+
 df = pd.DataFrame(values)
 df.to_csv(f"{pca_results_folder}/features.csv", index=False)
+tsne = TSNE().fit_transform(df[["Max Eigen Ratio", "Variance of Eigen Ratio", "Normalized Area Under Eigen Ratio"]].values)
+print(tsne.shape)
+
+plt.scatter(tsne[:, 0], tsne[:, 1])
+plt.title("Tsne: Max Eigen Ratio, Variance and Area Under ER Curve")
+plt.xlabel("Tsne Axis 1")
+plt.ylabel("Tsne Axis 2")
+plt.savefig(f"{pca_results_folder}/tsne.jpg")
